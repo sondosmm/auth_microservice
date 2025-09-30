93dc65d7ff53b93281f8c2b0062346c8c0deb9f0
sondosmm <sondosmm6@gmail.com>
2025-09-30 18:18:15 +0300

    edit readme file with new feature

--- auth/app.js
+++ auth/app.js
@@ -1,25 +1,22 @@
-const express= require('express');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const connectDB= require('./config/db');
-const authRoutes = require('./routes/authRoutes');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
-const app= express();
+const app = express();
 
-
-
-app.use(cors({origin:'http://localhost:5000',credentials:true}));
+app.use(cors({ origin: "http://localhost:5000", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
-app.use((err,req,res,next)=>{
-    console.error(err.stack);
-    res.status(500).json({error:'Something broke'});
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something broke" });
 });
 
-app.use('/api/auth',authRoutes);
+app.use("/api/auth", authRoutes);
 
 connectDB();
 
-
-module.exports=app;
+module.exports = app;
