d1e23c1b325e1280ab9a7bb0088cfd455015a735
sondosmm <sondosmm6@gmail.com>
2025-09-30 17:23:21 +0300

    added logout function

--- auth/app.js
+++ auth/app.js
@@ -13,11 +13,11 @@ app.use(cors({origin:'http://localhost:5000',credentials:true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use((err,req,res,next)=>{
-    console.error(err.stock);
+    console.error(err.stack);
     res.status(500).json({error:'Something broke'});
 });
 
-app.use('/api/auth',authRoutes);
+app.use('/api',authRoutes);
 
 connectDB();
 
--- auth/routes/authRoutes.js
+++ auth/routes/authRoutes.js
@@ -1,17 +1,20 @@
-const express= require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
 
-const authController= require('../controllers/authController');
-const auth= require('../middleware/auth');
-const asyncHandler= require('../utils/asyncHandler');
+const authController = require("../controllers/authController");
+const auth = require("../middleware/auth");
+const asyncHandler = require("../utils/asyncHandler");
 
+router.post("/register", asyncHandler(authController.register));
+router.post("/login", asyncHandler(authController.login));
+router.post("/logout", asyncHandler(authController.logout));
+router.post("/refresh", asyncHandler(authController.refresh));
+router.get(
+  "/protected",
+  auth,
+  asyncHandler((req, res) => {
+    res.json({ message: "Secure data", userId: req.userId });
+  })
+);
 
-router.post('/register',asyncHandler(authController.register));
-router.post('/login', asyncHandler(authController.login));
-router.post('/refresh', asyncHandler(authController.refresh));
-router.get('/protected', auth,asyncHandler((req,res)=>{
-    res.json({message:'Secure data', userId:req.userId});
-
-}));
-
-module.exports=router;
+module.exports = router;
